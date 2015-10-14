d7c2fa0c2d9a22b89db285954501465379160dbf
Romi Petrelis <romipetrelis@gmail.com>
2015-10-14 17:07:59 -0500

    using react-router

---
+++ src/routes.js
@@ -0,0 +1,15 @@
+"use strict";
+var React = require("react");
+var Router = require("react-router");
+var DefaultRoute = Router.DefaultRoute;
+var Route = Router.Route;
+
+var routes = (
+  <Route name="app" path="/" handler={require('./components/app')}>
+    <DefaultRoute handler={require('./components/homePage')} />
+    <Route name="instrument" handler={require('./components/instrument/instrumentPage')}/>
+    <Route name="about" handler={require('./components/about/aboutPage')}/>
+  </Route>
+);
+
+module.exports = routes;
